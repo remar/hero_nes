6368d8174611791459d4951fdd79dcd16f7b83f5
Andreas Remar <andreas.remar@gmail.com>
2012-11-07 08:47:23 +0100

    Read controller and move player around.

--- hero.asm
+++ hero.asm
@@ -38,7 +38,9 @@ NMI:
   STA $2003  ; set the low byte (00) of the RAM address
   LDA #$02
   STA $4014  ; set the high byte (02) of the RAM address, start the transfer
-  
+
+	.include "get_input.inc"
+	
   RTI        ; return from interrupt
  
 ;;;;;;;;;;;;;;  
