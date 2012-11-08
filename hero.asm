7bc8b464bfbff50d29146382e0e9616528d221e9
Andreas Remar <andreas.remar@gmail.com>
2012-11-08 07:46:51 +0100

    Use subroutine for reading out the controllers state.

--- hero.asm
+++ hero.asm
@@ -3,7 +3,15 @@
   .inesmap 0   ; mapper 0 = NROM, no bank swapping
   .inesmir 1   ; background mirroring
   
+  .rsset $0000  ;;start variables at ram location 0
+buttons1   .rs 1  ; player 1 gamepad buttons, one bit per button
 
+BTN_A = %10000000
+BTN_B = %01000000
+UP    = %00001000
+DOWN  = %00000100
+LEFT  = %00000010
+RIGHT = %00000001
 ;;;;;;;;;;;;;;;
 
     
@@ -19,8 +27,16 @@
   STA $0203        ; put sprite 0 in center ($80) of screen horiz
   LDA #$00
   STA $0201        ; tile number = 0
-	LDA #%01000000
+  LDA #%00000000
   STA $0202        ; color = 0, no flipping
+
+  LDA #$60
+  STA $0204        ; put sprite 1 at $60 vert
+  STA $0207        ; put sprite 1 at $60 horiz
+  LDA #$01
+  STA $0205        ; tile number = 1
+  LDA #%00000000
+  STA $0206        ; color = 0, no flipping
 
   LDA #%10000000   ; enable NMI, sprites from Pattern Table 0
   STA $2000
@@ -39,10 +55,24 @@ NMI:
   LDA #$02
   STA $4014  ; set the high byte (02) of the RAM address, start the transfer
 
+	LDA $0207 			; get bullet x
+	CLC
+	ADC #$02
+	STA $0207
+
+	JSR ReadController1
+
+	.include "reada.inc"
+	.include "readb.inc"
+	.include "readup.inc"
+	.include "readdown.inc"
+	.include "readleft.inc"
+	.include "readright.inc"
+
+	RTI        ; return from interrupt
+ 
 	.include "get_input.inc"
 	
-  RTI        ; return from interrupt
- 
 ;;;;;;;;;;;;;;  
   
   .bank 1
@@ -63,4 +93,4 @@ NMI:
   
   .bank 2
   .org $0000
-  .incbin "hero.chr"   ;includes 8KB graphics file from SMB1
+  .incbin "hero.chr"   ;includes 8KB graphics file
